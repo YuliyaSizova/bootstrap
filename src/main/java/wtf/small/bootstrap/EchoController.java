5e6d95156611d17655023896a7bfca2ed3293c27
Gennady <gennady.lebedev@gmail.com>
2017-04-08 22:35:29 +0400

    add swagger docs for echo controller

--- src/main/java/wtf/small/bootstrap/EchoController.java
+++ src/main/java/wtf/small/bootstrap/EchoController.java
@@ -1,28 +1,54 @@
 package wtf.small.bootstrap;
 
+import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.Map;
 
+@Api(value = "sample-controller", description = "Controller with a couple of examples. For testing purposes only")
 @RestController
 public class EchoController {
     private static Logger log = LoggerFactory.getLogger(EchoController.class);
 
-    @RequestMapping("/time")
+    @ApiOperation(
+            value = "time",
+            notes = "Returns current time as a string",
+            produces = MediaType.TEXT_PLAIN_VALUE
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Current time as a string", response = String.class)
+    })
+    @GetMapping("/time")
     public String time() {
         String now = LocalDateTime.now().toString();
         log.debug("Passing time {}", now);
         return now;
     }
 
-    @RequestMapping(value = "/echo", method = RequestMethod.POST)
-    public Map<String, Object> echo(@RequestBody Map<String, Object> body) {
+    @ApiOperation(
+            value = "echo",
+            notes = "Returns an echo of a posted JSON",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Parsed and returned JSON from a POST body"),
+            @ApiResponse(code = 500, message = "Error if request was incorrect")
+    })
+    @PostMapping(value = "/echo")
+    public Map<String, Object> echo(
+            @ApiParam(name = "body", value = "POST body as a JSON", required = true, example = "{\n" +
+                    "\t\"answer\": 42,\n" +
+                    "\t\"question\": \"unknown\",\n" +
+                    "\t\"composite\": {\n" +
+                    "\t\t\"something\": \"something\"\n" +
+                    "\t}\n" +
+                    "}")
+            @RequestBody Map<String, Object> body) {
         log.debug("Echoing data: {}", body);
         return body;
     }
